fa639458ee66696afaa5cde7918e6803dc162e39
Alone <hi@anlo.ng>
2025-10-31 18:59:57 +0800

    📈 AkTools MCP Server

--- mcp_aktools/__init__.py
+++ mcp_aktools/__init__.py
@@ -14,7 +14,7 @@ from .cache import CacheKey
 
 _LOGGER = logging.getLogger(__name__)
 
-mcp = FastMCP(name="mcp-aktools", version="0.1.9")
+mcp = FastMCP(name="mcp-aktools", version="0.1.10")
 
 field_symbol = Field(description="股票代码")
 field_market = Field("sh", description="股票市场，仅支持: sh(上证), sz(深证), hk(港股), us(美股), 不支持加密货币")
